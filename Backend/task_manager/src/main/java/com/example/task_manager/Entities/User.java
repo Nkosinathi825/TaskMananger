20d14360ec9bf8ca0aeb7372636a434d1b4c874f
Nkosinathi825 <nkosinathiphiri825@gmail.com>
2024-12-08 19:00:58 +0200

    added the user entity

---
+++ Backend/task_manager/src/main/java/com/example/task_manager/Entities/User.java
@@ -0,0 +1,70 @@
+package com.example.task_manager.Entities;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import javax.persistence.Column;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password; 
+
+    @NotNull
+    @Email
+    @Column(unique = true) 
+    private String email;
+
+    //construtors
+    public User() {}
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
